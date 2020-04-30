872beb442dfb5040b6d6f80d11911e67adb25c8b
BHAVESH4343 <63332504+BHAVESH4343@users.noreply.github.com>
2020-04-30 09:07:03 +0530

    Struct.c

---
+++ Struct.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct employee
+{
+	char EmpName[50];
+  char DeptName[50];
+  int EmpNo,Salary;
+};
+  int main()
+{
+int i,p=1;
+struct employee e;
+for(i=0;i<=2;i++)
+{
+	printf("Enter the data for Employee - %d\n",p);
+	p++;
+	scanf("%d\n",&e.EmpNo);
+	scanf("%s\n",e.EmpName);
+	scanf("%s\n",e.DeptName);
+	scanf("%d",&e.Salary);
+	printf("EmpNo=%d\n",e.EmpNo);
+	printf("EmpName=%s\n",e.EmpName);
+	printf("DeptName=%s\n",e.DeptName);
+	printf("Salary=%d\n\n",e.Salary);
+	fflush(stdin);
+}
+
+return 0;
+}
